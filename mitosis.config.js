99a45b346eb50aef842b1c3fb1382ac13a7d60a4
Rickie Zheng <rqzheng2008@126.com>
2023-11-30 19:49:16 +0800

    feat: update vue3 option in mitosis config

--- mitosis.config.js
+++ mitosis.config.js
@@ -1,19 +1,20 @@
 /** @type {import('@builder.io/mitosis').MitosisConfig} */
 module.exports = {
-  files: 'src/**',
-  targets: ['solid', 'react', 'vue3'],
-  dest: "dist",
-  options: {
-    solid: {
-      typescript: true,
-      stylesType: 'styled-components'
-    },
-    react: {
-      typescript: true,
-      stylesType: "style-tag",
-    },
-    vue3:{
-      typescript: true,
+    files: 'src/**',
+    targets: ['solid', 'react', 'vue3'],
+    dest: "dist",
+    options: {
+        solid: {
+            typescript: true,
+            stylesType: 'styled-components'
+        },
+        react: {
+            typescript: true,
+            stylesType: "style-tag",
+        },
+        vue3: {
+            typescript: true,
+            api: "composition"
+        }
     }
-  }
 };
